30c60f2f00ccf9ece2d7f78905c7bc16b3d70dac
Erickz1n <erickmso0811@gmail.com>
2023-08-28 22:24:12 -0300

    add button back-to-top and reveal elements

--- script.js
+++ script.js
@@ -1,3 +1,92 @@
+/* MENU ATIVO */
+
+const scrollReveal = ScrollReveal({
+    origin: 'top',
+    distance: '40px',
+    duration: 700,
+    // reset: true
+})
+
+scrollReveal.reveal(
+    `#form .title, #form .text, #form input, #form textarea, #form button, #footer .text, #footer .link`,
+    { interval: 100}
+    )
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+        const menuLinks = document.querySelectorAll("nav li a");
+        const sections = document.querySelectorAll("section");
+    
+        function setActiveLink() {
+        const scrollPosition = window.scrollY;
+    
+        sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        const sectionId = section.getAttribute("id");
+    
+        if (scrollPosition >= sectionTop - 50 && scrollPosition < sectionTop + sectionHeight - 50) {
+            menuLinks.forEach(link => {
+            link.classList.remove("active");
+            if (link.getAttribute("href") === `#${sectionId}`) {
+            link.classList.add("active");
+            }
+            });
+        }
+        });
+        }
+    
+        window.addEventListener("scroll", setActiveLink);
+    });
+    
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
+    
+    function changeHeaderWhenScroll() {
+        if (window.scrollY >= navHeight) {
+            // scroll é maior que a altura do header
+    
+            header.classList.add('scroll')
+        } else {
+            // menor que a altura do header
+            header.classList.remove('scroll')
+        }
+    }
+
+    const backToTopButton = document.querySelector('.back-to-top')
+
+    function backToTop() {
+        if (window.scrollY >= 560) {
+            backToTopButton.classList.add('show')
+        } else {
+            backToTopButton.classList.remove('show')
+        }
+    }
+
+    const sections = document.querySelectorAll('main section[id]')
+    function activateMenuAtCurrentSection() {
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for (const section of sections) {
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if(checkpointStart && checkpointEnd) {
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.add('active')
+        } else {
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.remove('active')
+        }
+    }
+}
+
 // ABRE E FECHA O MENU QUANDO CLICAR NO ICONE: HAMBURGUER E X
 const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
@@ -18,21 +107,9 @@ for (const link of links) {
     })
 }
 
-// BOTAO BACK-TO-TOP
-
-const backToTopButton = document.querySelector('.back-to-top')
-
-function backToTop() {
-    if (window.scrollY >= 560) {
-        backToTopButton.classList.add('show')
-    } else {
-        backToTopButton.classList.remove('show')
-    }
-}
-
-
-window.addEventListener('scroll', function () {
-    changeHeaderWhenScroll()
-    backToTop()
-    activateMenuAtCurrentSection()
-})
+    
+    window.addEventListener('scroll', function () {
+        backToTop()
+        changeHeaderWhenScroll()
+        activateMenuAtCurrentSection()
+    })
