e0ef665513e9a77999d1bf059b05a1b01d5b081f
Erickz1n <erickmso0811@gmail.com>
2023-08-31 18:18:39 -0300

    add more inputs and API send to email

--- script-thanks.js
+++ script-thanks.js
@@ -8,7 +8,7 @@ const scrollReveal = ScrollReveal({
 })
 
 scrollReveal.reveal(
-    `#form .title, #form .text, #form input, #form select, #form textarea, #form button, #footer .text, #footer .link`,
+    `header, #thanks h1, #thanks .button, #form button, #footer .text, #footer .link`,
     { interval: 100}
     )
 
@@ -86,25 +86,6 @@ scrollReveal.reveal(
 
 // WHATSAPP INPUT
 
-const input = document.getElementById('telefone');
-
-function formatPhoneNumber() {
-    const value = input.value.replace(/\D/g, '');
-    let formattedValue = '';
-
-    if (value.length > 0) {
-        formattedValue += '(' + value.substring(0, 2);
-    }
-    if (value.length > 2) {
-        formattedValue += ') ' + value.substring(2, 7);
-    }
-    if (value.length > 7) {
-        formattedValue += '-' + value.substring(7, 11);
-    }
-
-    input.value = formattedValue;
-}
-
 
 // CLIQUE NO SANDUICHE PARA ELE SUMIR
 
@@ -130,83 +111,6 @@ for (const link of links) {
         nav.classList.remove('show')
     })
 }
-
-
-// SUBMIT
-
-class FormSubmit {
-    constructor(settings) {
-        this.settings = settings;
-        this.form = document.querySelector(settings.form);
-        this.formButton = document.querySelector(settings.button);
-        if (this.form) {
-            this.url = this.form.getAttribute("action");
-        }
-        this.sendForm = this.sendForm.bind(this);
-    }
-
-    displaySuccess() {
-        this.form.innerHTML = this.settings.success;
-    }
-
-    displayError() {
-        this.form.innerHTML = this.settings.error;
-    }
-
-    getFormObject() {
-        const formObject = {};
-        const fields = this.form.querySelectorAll("[name]");
-        fields.forEach((field) => {
-            formObject[field.getAttribute("name")] = field.value;
-        });
-        return formObject;
-    }
-
-    onSubmission(event) {
-        event.preventDefault();
-        event.target.disabled = true;
-        event.target.innerText = "Enviando...";
-    }
-
-    async sendForm(event) {
-        try {
-            this.onSubmission(event);
-            await fetch(this.url, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
-                },
-                body: JSON.stringify(this.getFormObject()),
-            });
-
-            // Adicionar um atraso de 2 segundos (2000 milissegundos) antes do redirecionamento
-            setTimeout(() => {
-                window.location.href = "thanks.html";
-            }, 1000); // Tempo em milissegundos (2 segundos)
-
-        } catch (error) {
-            this.displayError();
-            throw new Error(error);
-        }
-    }
-
-    init() {
-        if (this.form) this.formButton.addEventListener("click", this.sendForm);
-        return this;
-    }
-}
-
-const formSubmit = new FormSubmit({
-    form: "[data-form]",
-    button: "[data-button]",
-    success: "<h1 class='success'>Mensagem enviada!</h1> ", 
-    error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
-});
-
-formSubmit.init();
-
-
 
     
     
